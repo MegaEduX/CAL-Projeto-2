d1849f6d8fdb831c7d9e9fcdfb7833903d73c7ec
Eduardo Almeida <megaedux@me.com>
2014-05-11 20:21:38 +0100

    Initial Commit

---
+++ cal_tp02/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cal_tp02
+//
+//  Created by Eduardo Almeida on 11/05/14.
+//  Copyright (c) 2014 Bitten Apps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
